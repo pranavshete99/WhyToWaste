11124c6eb421e59b1fbf4c7326c4c84934e6d02d
pranavshete99 <pranavs0904@gmail.com>
2024-10-08 13:06:43 +0530

    first commit

---
+++ components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='w-full h-full bg-slate-500'>
+      <div>
+        <nav>
+
+        </nav>
+      </div>
+      <div className='font-bold'>
+        Impact
+      </div>
+      <div>
+        services
+      </div>
+      <div>
+        work
+      </div>
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Home
